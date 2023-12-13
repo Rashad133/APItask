1bd9e097b110cd30619cc00434707b5f87223abd
Rashad133 <tu82907hw@code.edu.az>
2023-12-14 02:33:39 +0400

    API intro task

---
+++ APItask/Controllers/TagsController.cs
@@ -0,0 +1,74 @@
+﻿using APItask.DAL;
+using APItask.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace APItask.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TagsController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+        public TagsController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Get(int page=1,int take=3)
+        {
+            List<Tag> tags= await _db.Tags.Skip((page-1)*take).Take(take).ToListAsync();
+            return Ok(tags);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            if (id <= 0) return StatusCode(StatusCodes.Status400BadRequest);
+
+            Tag tag = await _db.Tags.FirstOrDefaultAsync(t=>t.Id==id);
+
+            if(tag is null) return StatusCode(StatusCodes.Status404NotFound);
+
+            return StatusCode(StatusCodes.Status200OK);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Tag tag)
+        {
+            await _db.Tags.AddAsync(tag);
+            await _db.SaveChangesAsync();
+
+            return StatusCode(StatusCodes.Status201Created);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id,string name)
+        {
+            if(id<=0) return StatusCode(StatusCodes.Status400BadRequest);
+            Tag existed = await _db.Tags.FirstOrDefaultAsync(t=>t.Id==id);
+            if (existed is null) return StatusCode(StatusCodes.Status404NotFound);
+
+            existed.Name = name;
+            await _db.SaveChangesAsync();
+
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            if (id <= 0) return StatusCode(StatusCodes.Status400BadRequest);
+
+            Tag existed= await _db.Tags.FirstOrDefaultAsync(t=>t.Id == id);
+
+            if (existed is null) return StatusCode(StatusCodes.Status404NotFound);
+
+             _db.Tags.Remove(existed);
+            await _db.SaveChangesAsync();
+
+            return NoContent();
+
+        }
+    }
+}
---
+++ APItask/DAL/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using APItask.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace APItask.DAL
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+            
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
